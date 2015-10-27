045cdcc6fbf6f6bc29cfc1ada589a9a80c8b50fb
Hoa Ly <lythaihoa2311992@gmail.com>
2015-10-26 23:40:50 -0700

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,9 +1,9 @@
+from swap import swap 
+
 def insertion_sort(s): 
     for i in range(1,len(s):
         k = i
         while k > 1 and s[k] < s[k-1]: 
-            temp = s[k]
-            s[k] = s[k-1]
-            s[k-1] = temp 
+            swap(s,k,k-1)
             k = k-1
     return s
