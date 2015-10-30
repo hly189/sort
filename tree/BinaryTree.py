0454a5c36e0ffb96540b481bd2444ef711d2d00d
Hoa Ly <lythaihoa2311992@gmail.com>
2015-10-29 20:54:53 -0700

    adding find function

--- tree/BinaryTree.py
+++ tree/BinaryTree.py
@@ -1,20 +1,47 @@
+class Node: 
+	def __init__(self, val,key):
+		self.key = key 
+		self.value = val
+		self.left = None 
+		self.right = None 
 
-class BinaryTree(object):
-	
+	def get_left(self):
+		return self.left
+
+	def get_right(self): 
+		return self.right
+
+	def set_right_equal(self,data): 
+		self.right = data
+
+	def set_left_equal(self,data): 
+		self.left = data
+
+	def count(self): 
+		count = 0
+		if self.left is not None: 
+			count = count + 1
+		if self.right is not None: 
+			count =  count + 1
+
+	def find(self,key): 
+		if self.item == key: 
+			return False
+		else: 
+			if self.left is not None: 
+				return self.left.find(key)
+			else: 
+				return self.right.find(left)
+
+
+class BinaryTree(object):	
 	def __init__(self,item): 
 		self.item = item 
-		self.left_child = None 
-		self.right_child = None
+		self.root = None
 		self.size = 0
 
 	def get_size(self):
 		return self.size  
-
-	def get_left_child(self):
-		return self.left_child
-
-	def get_right_child(self): 
-		return self.right_child 
 
 	def get_item(self): 
 		return self.item
@@ -22,32 +49,31 @@ class BinaryTree(object):
 	def set_item_equal(self, data): 
 		self.item = data 
 
-	def count(self): 
-		count = 0
-		if self.left_child:
-			count = count +1 
-		if self.right_child:
-			count = count + 1
-		return count 
-
-	def insert_right(self, new_node): 
-		if self.right_child is None: 
-			self.right_child = BinaryTree(new_node)
+	def insert_right(self, key): 
+		current = self.root 
+		if self.right is None: 
+			self.right = BinaryTree(key)
 		else: 
-			tree = BinaryTree(new_node)
-			tree.right_child = self.right_child
-			self.right_child = tree
-		self.size = self.size +1
-
-	def insert_left(self, new_node): 
-		if self.left_child is None: 
-			self.left_child = BinaryTree(new_node)
-		else: 
-			tree = BinaryTree(new_node)
-			tree.left_child = self.left_child
-			self.left_child = tree
+			tree = BinaryTree(key)
+			tree.right = self.right
+			self.right = tree
 		self.size = self.size + 1
-	
+
+	def insert_left(self, key): 
+		if self.left is None: 
+			self.left = BinaryTree(key)
+		else: 
+			tree = BinaryTree(key)
+			tree.left = self.left
+			self.left= tree
+		self.size = self.size + 1
+
+	def find(self,key): 
+		if self.root is not None: 
+			return self.root.find(key)
+		else: 
+			return False 
+
 def printTree(tree): 
 	if tree != None:  
 		printTree(tree.get_left_child())
