e3e4ecd1a7013a5c29f98c2996bae5e2f8947ee7
Hoa Ly <lythaihoa2311992@gmail.com>
2015-11-01 00:15:02 -0700

    This is the method for kth most frequent number in an array

--- count_element.py
+++ count_element.py
@@ -2,6 +2,11 @@ from counting import remove
 from quick_sort import quick_sort
 import random 
 
+"""
+This function will be used to count repeated element in an array 
+The return will be an array of count number, and it will also print 
+out each element of array and repeated time of that element
+"""
 def counting_element(s): 
 	#if array is empty, force quit 
 	if len(s) == 0: return 
@@ -28,12 +33,21 @@ def counting_element(s):
 	print("element:{0},appear:{1}".format(previous,count))
 	return new_array
 
+"""
+This function will sort an array and deleted the duplicated of an element that array 
+"""
 def remove_sort(s): 
 	s = remove(quick_sort(s))
 	return s 
 
+"""
+This functino is used to map 2 arrays into a dictionary
+"""
 def put_to_dict(s1,s2): 
-	dictionary = dict()
+	dictionary = dict(zip(s1,s2))
+	return dictionary
+
+def 
 
 
 s = [1,3,2,4,2]
