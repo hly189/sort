df97943e69b220f40e85b8e218b0a3393b8e83a5
Hoa Ly <lythaihoa2311992@gmail.com>
2015-10-31 21:37:38 -0700

    needle in haystack

---
+++ haystack_needle.py
@@ -0,0 +1,22 @@
+def strStr(haystack, neddle): 
+	if len(haystack) < len(neddle): return None 
+	i = 0
+
+	while i < len(haystack) - len(neddle) + 1: 
+		j = 0
+		k = i 
+		while j < len(neddle): 
+			if haystack[k] == neddle[j]: 
+				j = j + 1
+				k = k+1
+			else: 
+				break 
+		if j == len(neddle):
+			break
+		else: 
+			i = i + 1
+	if i == len(haystack) - len(neddle) + 1: 
+		print("no substring")
+	else: 
+		return haystack[i:]
+
